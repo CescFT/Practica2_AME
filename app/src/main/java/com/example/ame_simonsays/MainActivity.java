f0584077b1eb2e37c69724b19dee54e99146b5c1
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2020-01-19 11:19:27 +0100

    master: modifications.

--- app/src/main/java/com/example/ame_simonsays/MainActivity.java
+++ app/src/main/java/com/example/ame_simonsays/MainActivity.java
@@ -2,32 +2,77 @@ package com.example.ame_simonsays;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    /**
+     * Elements que necessitem per a executar el codi
+     */
+    private TextView mTextView;
+    private int indexSeq;
+    private Button botoBlau;
+    private Button botoVermell;
+    private Button botoVerd;
+    private Button botoGroc;
+    private List<colors> colorsDisponibles = new ArrayList<colors>();
+    List<colors> sequencia  = new ArrayList<colors>();
+    int sequenceLength;
+    private SharedPreferences sharedPrefs;
+    private SharedPreferences.Editor sharedPrefsEditor;
+
+
+    /**
+     * Classe privada per als colors
+     */
+    private enum colors{
+        BLAU,
+        GROC,
+        VERD,
+        VERMELL
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        List<colors> sequencia  = new ArrayList<colors>();
-        List<colors> colorsDisponibles = new ArrayList<colors>();
+    /**
+     * Mètode que inicialitza la llista de colors disponibles
+     */
+    private void iniLlista(){
         colorsDisponibles.add(colors.BLAU);
         colorsDisponibles.add(colors.GROC);
         colorsDisponibles.add(colors.VERD);
         colorsDisponibles.add(colors.VERMELL);
-        int sequenceLength = 5 + (int)(Math.random() * ((10 - 5) + 1));  // [5,10]
+    }
+
+    private boolean comprovarValor(TextView t, String text){
+        return t.getText().toString().equals(text);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView =(TextView) findViewById(R.id.seg_color);
+        indexSeq =0;
+        botoBlau =  (Button) findViewById(R.id.botoBlau);
+        botoVermell = (Button) findViewById(R.id.botoVermell);
+        botoVerd = (Button) findViewById(R.id.botoVerd);
+        botoGroc = (Button) findViewById(R.id.botoGroc);
+
+        sharedPrefs = getPreferences(MODE_PRIVATE);
+        sharedPrefsEditor = sharedPrefs.edit();
+
+        this.iniLlista();
+
+        sequenceLength = 5 + (int)(Math.random() * ((10 - 5) + 1));  // [5,10]
 
         //inicialització de la seqüència que tindrà el joc.
         for(int i=0; i<sequenceLength; i++)
@@ -39,30 +84,43 @@ public class MainActivity extends AppCompatActivity {
         if(!sequencia.isEmpty())
         {
             //anar agafant els valors de la llista, pintarlos al textview i esperar a que apreti boto, comprovar q esta be i incrementar la variable índex.
-            int indexSeq=0;
-            boolean botoBlauApretat = false;
-            boolean botoVermellApretat = false;
-            boolean botoVerdApretat = false;
-            boolean botoGrocApretat = false;
-            TextView mTextView = (TextView) findViewById(R.id.seg_color);
-            Button botoBlau = (Button) findViewById(R.id.botoBlau);
-            Button botoVermell = (Button) findViewById(R.id.botoVermell);
-            Button botoVerd = (Button) findViewById(R.id.botoVerd);
-            Button botoGroc = (Button) findViewById(R.id.botoGroc);
 
 
+            botoBlau.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 
-            
+                }
+            });
+
+
+            botoGroc.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+
+                }
+            });
+
+            botoVerd.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+
+                }
+            });
+
+            botoVermell.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+
+                }
+            });
 
 
             while(indexSeq < sequenceLength)
             {
                 //mentre hi ha joc
-
                 mTextView.setText(sequencia.get(indexSeq).toString());
 
             }
         }
-
     }
 }
--- app/src/main/java/com/example/ame_simonsays/colors.java
+++
@@ -1,8 +0,0 @@
-package com.example.ame_simonsays;
-
-public enum colors {
-    BLAU,
-    VERD,
-    VERMELL,
-    GROC
-}
